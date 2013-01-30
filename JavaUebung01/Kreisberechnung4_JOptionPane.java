3329c768388efdfc021dd5a0882bad6bb028bce8
Denis Benaissa <d.benaissa@gmx.de>
2013-01-30 15:30:29 +0100

    Kreisberechnung ohne Konsole

---
+++ JavaUebung01/Kreisberechnung4_JOptionPane.java
@@ -0,0 +1,21 @@
+// Kreisberechnung ohne Konsole
+
+import javax.swing.JOptionPane;
+
+public class Kreisberechnung4_JOptionPane {
+	public static void main(String[] args) {
+		double radius, umfang, flaeche;
+		String einheit, eingabe;
+		eingabe = JOptionPane.showInputDialog("Geben Sie den Kreisradius ein: ");
+		radius = Double.parseDouble(eingabe);
+		System.out.print("Geben Sie die Einheit ein: ");
+		eingabe = JOptionPane.showInputDialog("Geben Sie die Einheit ein: ");
+		einheit = eingabe;
+		umfang = 2.0 * 3.1415926 * radius;
+		flaeche = 3.1415926 * radius * radius;
+		JOptionPane.showMessageDialog(
+			null,"Umfang: " + umfang + " "
+			+ einheit + "\nFläche: " + flaeche + " "
+			+ einheit + '\u00b2');
+	}
+}
