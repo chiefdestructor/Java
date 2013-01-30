f21f490cc52e93cc5ef86cc1035320ea50f549e1
Denis Benaissa <d.benaissa@gmx.de>
2013-01-30 10:50:22 +0100

    Kreisberechnung mit Eingabefenster

---
+++ JavaUebung01/Kreisberechnung4.java
@@ -0,0 +1,25 @@
+/* Kreisberechnung: Der Radius für einen Kreis und die Einheit
+* werden über die Tastatur eingegeben. Anschließend 
+* werden der Umfang und der Flächeninhalt berechnet
+*/
+
+import javax.swing.*;
+
+public class Kreisberechnung4 {
+    public static void main (String[] args){
+	    double radius, umfang, flaeche;
+		String eingabe, einheit;
+		eingabe = JOptionPane.showInputDialog("Geben Sie den Kreisradius ein: ");
+		radius = Double.parseDouble(eingabe);
+		eingabe = JOptionPane.showInputDialog("Geben Sie die Einheit ein: ");
+		einheit = eingabe;
+		umfang = 2.0 * 3.1415926 * radius;
+		flaeche = 3.1415926 * radius * radius;
+		System.out.print("Umfang: ");
+		System.out.print(umfang);
+		System.out.println(" " + einheit);
+		System.out.print("Flaeche: ");
+		System.out.print(flaeche);
+		System.out.println(" " + einheit + '\u00fd');
+	}
+}
